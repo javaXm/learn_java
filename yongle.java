5ad914f08884fee3028032b133bf5b3a7f3fa9bf
javaXm <jenniferx617@gmail.com>
2017-07-16 22:58:36 -0500

    Update yongle.java

--- yongle.java
+++ yongle.java
@@ -1,1 +1,11 @@
+public class task02 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 3) {
+            System.out.println("Yongle Zhen Shuai!");
+            i++;
 
+        }
+    }
+
+}
